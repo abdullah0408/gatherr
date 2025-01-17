ec49b27956925c2ae5455b1592e2b5a873b2bf26
abdullah <abdullah981066@gmail.com>
2025-01-17 21:05:09 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    staleTimes: { dynamic: 30 }, // This experimental feature allows dynamic content to be cached for 30 seconds before it's considered stale and fetched again.
+  },
+  images: {
+    domains: ["*"], // Allow images from all external domains
+  },
 };
 
 export default nextConfig;
