bfca53727279ff216280b3afa3cfdddc92201487
abdullah <abdullah981066@gmail.com>
2025-01-20 20:12:11 +0530

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import SignOut from '../components/SignOut';
+import Link from "next/link";
 
 export default function Home() {
   const { setTheme } = useTheme();
@@ -60,6 +61,7 @@ export default function Home() {
       ) : (
         <p>Loading user details...</p>
       )}
+      <Link href="/hello">Hello</Link>
     </>
   );
 }
